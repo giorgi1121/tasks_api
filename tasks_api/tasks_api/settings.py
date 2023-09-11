8d1db4ca022f7b10efbaee86f7da3ef8ec1d0f18
Giorgi Abashidze <giorgi.abashidze2@gmail.com>
2023-09-11 16:57:27 +0400

    Modified DEBUG and ALLOWED_HOSTS variables - In settings.py we modified DEBUG variable from True to False. - In settings.py we modified ALLOWED_HOSTS variable from nothing to *;

--- tasks_api/tasks_api/settings.py
+++ tasks_api/tasks_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)c1+%nz5qk4p4sra-=8v*-z7et7l)%v^mjr1bavo$@jpnjwi(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
