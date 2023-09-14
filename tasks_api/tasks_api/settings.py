3bfc6ffc61460a71b69456577e6a8fa56556249b
Giorgi Abashidze <giorgi.abashidze2@gmail.com>
2023-09-15 00:51:20 +0400

    Added staticfilesMiddleware

--- tasks_api/tasks_api/settings.py
+++ tasks_api/tasks_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.static.StaticFilesMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
